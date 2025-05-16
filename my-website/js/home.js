210e5b195c012f0451d15357112407946c0abeb6
Riodamian <riodamianxtop1@gmail.com>
2025-05-16 22:24:35 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,6 +1,6 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
-    const BASE_URL = 'https://api.themoviedb.org/3';
-    const IMG_URL = 'https://image.tmdb.org/t/p/original';
+const API_KEY = 'https://adsnetwork11001297.o18a.com/c?o=21631839&m=25262&a=672951&aff_click_id={A16}&sub_aff_id={A16}';
+    const BASE_URL = 'https://adsnetwork11001297.o18a.com/c?o=21631839&m=25262&a=672951&aff_click_id={A16}&sub_aff_id={A16}';
+    const IMG_URL = 'https://adsnetwork11001297.o18a.com/c?o=21631839&m=25262&a=672951&aff_click_id={A16}&sub_aff_id={A16}';
     let currentItem;
 
     async function fetchTrending(type) {
